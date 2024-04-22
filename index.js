b8fc6874eae006c671c5ac3b8ac54140ab0b4743
kaito02020424 <104062884+kaito02020424@users.noreply.github.com>
2024-04-22 00:59:28 +0000

    fix: running js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (async () => {
+    alert("hi")
     const res = await fetch("https://www.youtube.com")
+    const text = await res.text()
     const element = document.createElement("p")
-    element.textContent = await res.text()
+    element.textContent = text
     document.getElementById('textbox').appendChild(element)
-})
+    alert(text)
+})()
